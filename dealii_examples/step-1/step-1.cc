2625b783ad10496b5a4b925675c46a4675d7fb51
jauribep <jauribep@unal.edu.co>
2020-04-24 04:04:30 -0500

    Comenta lineas

--- dealii_examples/step-1/step-1.cc
+++ dealii_examples/step-1/step-1.cc
@@ -53,24 +53,24 @@ void second_grid()
   const double   inner_radius = 0.5, outer_radius = 1.0;
   GridGenerator::hyper_shell(
     triangulation, center, inner_radius, outer_radius, 10);
-  for (unsigned int step = 0; step < 5; ++step)
-    {
-      for (auto &cell : triangulation.active_cell_iterators())
-        {
-          for (unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v)
-            {
-              const double distance_from_center =
-                center.distance(cell->vertex(v));
-
-              if (std::fabs(distance_from_center - inner_radius) < 1e-10)
-                {
-                  cell->set_refine_flag();
-                  break;
-                }
-            }
-        }
-
-      triangulation.execute_coarsening_and_refinement();
+  // for (unsigned int step = 0; step < 5; ++step)
+  //   {
+  //     for (auto &cell : triangulation.active_cell_iterators())
+  //       {
+  //         for (unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v)
+  //           {
+  //             const double distance_from_center =
+  //               center.distance(cell->vertex(v));
+  //
+  //             if (std::fabs(distance_from_center - inner_radius) < 1e-10)
+  //               {
+  //                 cell->set_refine_flag();
+  //                 break;
+  //               }
+  //           }
+  //       }
+  //
+  //     triangulation.execute_coarsening_and_refinement();
     }
 
 
