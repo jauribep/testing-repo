b1dd8a07ff292d104b94ed1a8dbb748099071bab
jauribep <jauribep@unal.edu.co>
2020-04-30 06:03:52 -0500

    adding new grid

--- dealii_examples/step-3/step-3.cc
+++ dealii_examples/step-3/step-3.cc
@@ -91,8 +91,16 @@ Step3::Step3()
 
 void Step3::make_grid()
 {
-  GridGenerator::hyper_cube(triangulation, -1, 1);
-  triangulation.refine_global(5);
+  // GridGenerator::hyper_cube(triangulation, -1, 1);
+  // triangulation.refine_global(5);
+
+  GridGenerator::channel_with_cylinder
+  ( triangulation,
+    const double  	    shell_region_width = 0.03,
+    const unsigned int  n_shells = 2,
+    const double  	    skewness = 2.0,
+    const bool  	      colorize = false
+  );
 
   std::cout << "Number of active cells: " << triangulation.n_active_cells()
             << std::endl;
