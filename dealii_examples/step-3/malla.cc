d2fbecd16f7d676b820034334d9ef5332a9cf9cf
jauribep <jauribep@unal.edu.co>
2020-05-31 09:48:46 -0500

    otra

--- dealii_examples/step-3/malla.cc
+++ dealii_examples/step-3/malla.cc
@@ -291,7 +291,8 @@ namespace malla
     well_loc[0].push_back(well_loc_1);
     well_loc[1].push_back(well_loc_2);
 
-    std::cout << "well_loc" << well_loc[0][0] << std::endl;
+    std::cout << "well_loc0 " << well_loc[0][0] << std::endl;
+    std::cout << "well_loc0 " << well_loc[0][1] << std::endl;
 
     // //Bulk grid creation
     // Triangulation<2> bulk_tria;
