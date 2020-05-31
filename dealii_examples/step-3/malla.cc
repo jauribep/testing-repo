c85f987cf20bbf4f1ae1ba3efa2e97d9209e334d
jauribep <jauribep@unal.edu.co>
2020-05-31 14:58:21 -0500

    otra

--- dealii_examples/step-3/malla.cc
+++ dealii_examples/step-3/malla.cc
@@ -293,6 +293,8 @@ namespace malla
     well_loc[0].push_back(well_loc_1);
     well_loc[1].push_back(well_loc_2);
 
+    std::cout << "Hola" << std::endl;
+
     std::cout << "well_loc0 " << well_loc[0][0] << std::endl;
     std::cout << "well_loc0 " << well_loc[0][1] << std::endl;
     std::cout << "well_loc0 " << well_loc[1][0] << std::endl;
