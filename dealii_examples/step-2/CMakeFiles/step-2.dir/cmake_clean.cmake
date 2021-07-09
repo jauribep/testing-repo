f0ce38b345d9941d1b1badc3fd86217f31ece50a
jauribep <jauribep@unal.edu.co>
2021-07-09 00:50:36 +0000

    Resultados

---
+++ dealii_examples/step-2/CMakeFiles/step-2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-2.dir/step-2.cc.o"
+  "step-2"
+  "step-2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
